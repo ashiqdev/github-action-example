ba6038aa8de24249e6fb642363d9652d10e09be1
Ashiqur Rahman <ashiquejava@gmail.com>
2020-03-30 11:49:25 +0600

    feat: puppeteer is added

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,35 @@
+const puppeteer = require("puppeteer");
 const expect = require("chai").expect;
 const numbers = [1, 2, 3, 4, 5];
+
+const { puppeteerConfig, url } = require("./config.json");
+
+const { browserOpts, pageOpts, viewport } = puppeteerConfig;
 
 // multiply array items by 2
 const multiplyBy2 = numbers => numbers.map(number => number * 2);
 const result = multiplyBy2(numbers);
 
 describe("simple test", () => {
+  let page;
+
+  before(async () => {
+    global.browser = await puppeteer.launch(browserOpts);
+    page = await browser.newPage();
+    await page.setViewport(viewport);
+  });
+
+  it("should go to example.com", async () => {
+    await page.goto(url, pageOpts);
+    const pageUrl = await page.url();
+    expect(pageUrl).to.equals(url);
+  });
+
+  it("should scrape the currect header", async () => {
+    const header = await page.$eval("div h1", header => header.innerText);
+    expect(header).to.equal("Example Domain");
+  });
+
   it("should be an array", () => {
     expect(numbers)
       .to.be.an("array")
@@ -14,5 +38,9 @@ describe("simple test", () => {
 
   it("should multiply array items by 2", () => {
     expect(result).to.eql([2, 4, 6, 8, 10]);
+  });
+
+  after(async () => {
+    await browser.close();
   });
 });
